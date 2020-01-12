f4f8d103d0e2445cdf41582d1cd8ae5637a4fe84
Lily <46561378+lily-law@users.noreply.github.com>
2020-01-12 15:56:19 +0000

    a tidy up
    deleted Timer2.ino from root, moved Timer2.ino from Timer2 folder to root

--- Timer2.ino
+++ Timer2.ino
@@ -7,21 +7,35 @@
 
 //timer variables
 int key = 0;
-unsigned long buttonPressed = 0;
 boolean stopped = true;
 boolean timerRunning = false;
 
 // button stuff
 int longPress = 1000; // time in ms for long button press
 boolean buttonShortPressed = false;
+boolean buttonLongPressed = false;
+unsigned long buttonPressed = 0; // time button was pressed
 unsigned long startBounce;  //start of bounce time
 int kpdelay=500; //keypad de-bounce (ms)
 boolean bounceLockout = false;
+void debounceButton(int ms);
 
 int state = 0;  // 3 timer states, 0 = stopped (at zero), 1 = running, 2 = paused
 int mode = 0; // 0 menu, 1 stop watch, 2 pomodoro, 3 counter
 
 void resetValues();
+void displayTime(unsigned long ms, boolean flash);
+unsigned long lastTimeToDisp = 0; // for display time to compare change for flash
+unsigned long dotsFlashedOnMsAgo = 0; // for testing when last dots flashed
+void beep();
+uint8_t data[] = { 0x19, 0x1a, 0x1b, 0x1c }; 
+uint8_t dataSTOP[] = { 0x6D, 0x78, 0x3F, 0x73 }; 
+uint8_t dataPOMO[] = { 0x73, 0x3F, 0x55, 0x3F }; 
+uint8_t dataCONT[] = { 0x39, 0x3F, 0x54, 0x78 }; 
+
+/*
+ * 0x06: 1, 0x07: 7, 0x08: _, 
+ */
 
 // menu
 void menu();
@@ -37,16 +51,20 @@ int count = 0;
 
 // pomodoro
 void pomodoro();
-unsigned long pomodoroTimer;
-unsigned long pomodoroPaused;
-int breakTime = (5*60)*1000; // 5 mins
-int workTime = (25*60)*1000; // 25 mins
+unsigned long lastPomoTime = 0;
+unsigned long breakTime = 60000*5; // 5 mins
+unsigned long longBreakTime = 60000*30; // 30 mins
+unsigned long workTime = 60000*25; // 25 mins
+unsigned long pomodoroTimer = workTime;
+
 TM1637Display display(CLK, DIO);
 
 void setup()
 {
     pinMode(4, INPUT_PULLUP);
-    display.setBrightness(0x0f);
+    pinMode(5, OUTPUT);
+    //display.setBrightness(0x0f);
+    display.setBrightness(2);
     //reset LED display to zero
     display.showNumberDec(0, false);
 }
@@ -61,26 +79,32 @@ void loop()
       bounceLockout = false;
     }  
 
-    if (!bounceLockout) {
-        startBounce = millis();
-        bounceLockout = true;
+    if (!buttonShortPressed && !bounceLockout) {
         if (key == LOW) {
-            if (!buttonPressed) { // if button is pressed record when
+            if (!buttonPressed) { // record when first pressed
                 buttonPressed = millis();
             }
-            else if (millis() - buttonPressed > longPress) { // button long pressed
+            else if (millis() - buttonPressed > longPress && !buttonLongPressed) { // button long pressed
+                buttonLongPressed = true;
+                beep(1, 100, 100);
                 if (mode != 0) {
                     mode = 0;
                 }
                 else {
-                    mode = selected;
                     resetValues();
+                    mode = selected;
                 }
-                buttonPressed = 0;
+                debounceButton(100);
             }
         }
-        else if (buttonPressed > 0) { // button short pressed
+        else if (millis() - buttonPressed > longPress) {
+            buttonPressed = 0;
+            buttonLongPressed = false;
+        }
+        else if (buttonPressed > 0 && !bounceLockout) { // button short pressed
+            debounceButton(50);
             buttonShortPressed = true;
+            beep(1, 50, 50);
             buttonPressed = 0;
         }
     }
@@ -99,16 +123,28 @@ void loop()
         counter();
         break;
     }
-
+   
     delay(10);
 }
 
 void menu() {
     if (buttonShortPressed) {
-        selected %= 3 + 1; // cycle the 3 modes
+        selected %= 3; // cycle the 3 modes
+        selected += 1;
         buttonShortPressed = false;
     }
-    display.showNumberDec(selected, false);
+   // display.showNumberDec(selected, false);
+   switch(selected) {
+    case 1:
+    display.setSegments(dataSTOP);
+    break;
+    case 2:
+    display.setSegments(dataPOMO);
+    break;
+    case 3:
+    display.setSegments(dataCONT);
+    break;
+   }
 }
 
 void stopWatch() {
@@ -122,11 +158,11 @@ void stopWatch() {
     }
     if (state == 1) { //display the time
         //display seconds on the LED display
-        int rawSeconds = round((millis()-startMillis)/1000);
-        display.showNumberDec(rawSeconds, false);
+        unsigned long rawMs = millis()-startMillis;
+        displayTime(rawMs, true);
     }
     if (state == 0) { //reset the timer
-        display.showNumberDec(0, false);  //reset LED display to zero
+        displayTime(0, false);  //reset LED display to zero
     }  
 }
 
@@ -134,22 +170,29 @@ void pomodoro() {
     if (buttonShortPressed) {
         if (state != 1) {
             state = 1; // start timer
+            pomodoroTimer += millis();
         }
         else {
-            pomodoroPaused = millis();
             state = 2;
+            pomodoroTimer -= millis(); // pause so store time left
         }
         buttonShortPressed = false;
     }
-    if (state == 0) { // timer setup from now
-        pomodoroTimer = millis() + (count % 2 == 0 ? breakTime : workTime);
+      if (state == 0) { // stopped, set timer start
+          pomodoroTimer = ((count % 2) == 0) ? workTime : (count > 1 && ((count + 1) % 8) == 0) ? longBreakTime : breakTime;
+          displayTime(pomodoroTimer, false);
+      }
+      else if (state == 1) { // running
+          unsigned long remainingTime = pomodoroTimer - millis();
+          if (remainingTime < 100) { // timer finished
+            state = 0;
+            count += 1;
+            beep(15, 50, 25);
+          }
+          else {
+            displayTime(remainingTime, true);
+          }
     }
-    else if (state == 2) { // time left 
-        int remaining = pomodoroTimer - pomodoroPaused;
-        pomodoroTimer = millis() + remaining;
-    }
-    int pomodoroSeconds = round((pomodoroTimer - millis())/1000);
-    display.showNumberDec(count, false);
 }
 
 void counter() {
@@ -162,4 +205,40 @@ void counter() {
 
 void resetValues() {
     count = 0;
+    state = 0;
+}
+
+void displayTime(unsigned long ms, boolean flash) {
+  unsigned long seconds = round(ms/1000);
+  int secs = seconds % 60;
+  int mins = (seconds > 59) ? (floor(seconds / 60)*100) : 0;
+  int timeToDisp = mins + secs;
+  timeToDisp = timeToDisp > 9999 ? 9999 : timeToDisp;
+  int showDots = 0;
+  if (lastTimeToDisp != timeToDisp || !flash) {
+     showDots = 1;
+     dotsFlashedOnMsAgo = millis();
+  }
+  else if (millis() - dotsFlashedOnMsAgo < 500) {
+    showDots = 1;
+  }
+  display.showNumberDecEx(timeToDisp, (0x80 >> showDots), true);
+  lastTimeToDisp = timeToDisp;
+}
+
+void debounceButton(int ms) {
+  if (!bounceLockout) {
+    kpdelay = ms;
+    startBounce = millis();
+    bounceLockout = true;
+  }
+}
+
+void beep(int num, int len, int gap) {
+  for (int i=0; i<num; i++) {
+    analogWrite(5, 127);
+    delay(len);
+    analogWrite(5, 0);
+    delay(gap);
+  }
 }
